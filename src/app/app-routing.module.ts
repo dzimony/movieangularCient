65875880e362b1ce5ff99ff395186aed4b54cece
dzimony <dzimony@gmail.com>
2023-09-05 12:09:13 +0100

    approutupdate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,6 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { useHash: true })],  exports: [RouterModule]
 })
 export class AppRoutingModule { }
